8d34a654e23608655ac6ddfa5bdc4f9416776f3d
Valerio Arnaboldi <valerio.arnaboldi@gmail.com>
2020-03-11 12:14:56 -0700

    feature: blank target for references

--- frontend/src/pages/ReferenceElement.js
+++ frontend/src/pages/ReferenceElement.js
@@ -3,7 +3,7 @@ import React from 'react';
 class ReferenceElement extends React.Component {
     render() {
         return (
-            <a href={"https://wormbase.org/resources/paper/" + this.props.element}>{this.props.element}</a>
+            <a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + this.props.element}>{this.props.element}</a>
         );
     }
 }
