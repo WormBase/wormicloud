a80f5edc5892e1431c9daaf079dcf53960c1d4da
Valerio Arnaboldi <valearna@caltech.edu>
2020-03-17 10:28:28 -0700

    feat: custom tokenizer that recognizes gene names

--- backend/nlp.py
+++ backend/nlp.py
@@ -1,11 +1,15 @@
 from typing import List
-from nltk import RegexpTokenizer, WordNetLemmatizer, Counter
+from nltk import RegexpTokenizer, MWETokenizer, WordNetLemmatizer, Counter
 from backend.api import stop_words
+
+fixed_words = [('C.', 'elegans'), ('Caenorhabditis', 'elegans')]
+EXCLUSION_LIST = {'the', 'The', 'in', 'In', 'we', 'We'}
 
 
 def get_word_counts(corpus: List[str], count: int = None):
-    tokenizer = RegexpTokenizer(r'\w+')
+    tokenizer = RegexpTokenizer(r'[0-9a-zA-Z]+\-[0-9a-zA-Z]+|\S+')
     lemmatizer = WordNetLemmatizer()
-    abs_tokens = [lemmatizer.lemmatize(word).lower() for abstract in corpus for word in tokenizer.tokenize(abstract)
-                  if word not in stop_words and len(word) > 1]
+    abs_tokens = [lemmatizer.lemmatize(word).lower().replace('.', '').replace('\"', '').replace(',', '').replace(';', '')
+                  for abstract in corpus for word in MWETokenizer(fixed_words).tokenize(tokenizer.tokenize(abstract))
+                  if word not in stop_words and word not in EXCLUSION_LIST and len(word) > 1]
     return Counter(abs_tokens).most_common(n=count)
