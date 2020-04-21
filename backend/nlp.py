24377484c399384ea32f48f8023e743b984a125f
Valerio Arnaboldi <valearna@caltech.edu>
2020-04-21 12:40:04 -0700

    fix: filtered out words in exclusion list after lemmatization

--- backend/nlp.py
+++ backend/nlp.py
@@ -5,7 +5,6 @@ from backend.api import stop_words
 import yaml
 
 
-fixed_words = [('C.', 'elegans'), ('Caenorhabditis', 'elegans')]
 with open(os.path.join(os.getcwd(), "backend", "config.yml"), 'r') as stream:
     try:
         EXCLUSION_LIST = yaml.safe_load(stream)["exclusion_list"]
@@ -18,7 +17,8 @@ def get_word_counts(corpus: List[str], count: int = None):
     tokenizer = RegexpTokenizer(r'[0-9a-zA-Z]+\-[0-9a-zA-Z]+|\S+')
     lemmatizer = WordNetLemmatizer()
     abs_tokens = [lemmatizer.lemmatize(word).replace('.', '').replace('\"', '').replace(',', '').replace(';', '')
-                  .strip('()').replace("Caenorhabditis_elegans", "C_elegans")
-                  for abstract in corpus for word in MWETokenizer(fixed_words).tokenize(tokenizer.tokenize(abstract))
+                  .strip('()')
+                  for abstract in corpus for word in tokenizer.tokenize(abstract)
                   if word not in stop_words and word not in EXCLUSION_LIST and len(word) > 1]
+    abs_tokens = [abs_token for abs_token in abs_tokens if len(abs_token) > 1 and abs_token not in EXCLUSION_LIST]
     return Counter(abs_tokens).most_common(n=count)
