d91e685edaa4b8b90499068bc8762cf50bdc5a9e
Valerio Arnaboldi <valearna@caltech.edu>
2020-05-29 12:27:34 -0700

    feat(references): wbpaper ids added to download file

--- frontend/src/pages/ReferenceList.js
+++ frontend/src/pages/ReferenceList.js
@@ -37,7 +37,7 @@ class ReferenceList extends React.Component {
                             <Col sm={7}>
                                 <Button variant="light" onClick={() => {
                                     downloadFile(this.props.references.map((r) => r.index + '\t' + r.title + '\t' +
-                                        r.journal + '\t' + r.year).join('\n'), "references", "text/plain", "txt").then(r => {})}}
+                                        r.journal + '\t' + r.year + '\t' + r.wb_id).join('\n'), "references", "text/plain", "txt").then(r => {})}}
                                 >Download references</Button>
                             </Col>
                         </Row>
