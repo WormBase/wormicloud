013dbcee31c247ffcec43d2bde1887b9d3f89a79
Valerio Arnaboldi <valearna@caltech.edu>
2020-06-25 15:47:00 -0700

    feat(ui): sort references by field

--- frontend/src/pages/ReferenceList.js
+++ frontend/src/pages/ReferenceList.js
@@ -8,20 +8,28 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import {downloadFile} from "../lib/file";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormLabel from "react-bootstrap/FormLabel";
+import FormControl from "react-bootstrap/FormControl";
 
 class ReferenceList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            elemPerPage: 20
+            elemPerPage: 20,
+            sortBy: ''
         }
     }
     render() {
         const PaginatedReferences = withPaginatedList(ReferenceElement, (offset) => {
             return new Promise(((resolve, reject ) => {
-                if (this.props.references.length > offset) {
-                    resolve({elements: this.props.references.slice(offset, offset + this.state.elemPerPage),
-                        totNumElements: this.props.references.length});
+                let refs = this.props.references;
+                if (this.state.sortBy !== '') {
+                    refs = refs.slice().sort((a, b) => (a[this.state.sortBy] > b[this.state.sortBy]) ? 1 : -1);
+                }
+                if (refs.length > offset) {
+                    resolve({elements: refs.slice(offset, offset + this.state.elemPerPage),
+                        totNumElements: refs.length});
                 } else {
                     resolve({elements: [], totNumElements: 0});
                 }})
@@ -36,11 +44,24 @@ class ReferenceList extends React.Component {
                             </Col>
                         </Row>
                         <Row>
-                            <Col sm={7}>
+                            <Col sm={2}>
                                 <Button size="sm" variant="outline-primary" onClick={() => {
                                     downloadFile(this.props.references.map((r) => r.index + '\t' + r.title + '\t' +
                                         r.journal + '\t' + r.year + '\t' + r.wb_id + '\t' + r.pmid).join('\n'), "references", "text/plain", "csv").then(r => {})}}
                                 >Download references</Button>
+                            </Col>
+                            <Col sm={2}>
+                                <FormGroup controlId="exampleForm.ControlSelect1">
+                                    <FormLabel>Sort By:</FormLabel>
+                                    <FormControl as="select" onChange={(event) => this.setState({sortBy: event.target.value})}>
+                                        <option value=''>Relevance</option>
+                                        <option value='title'>Title</option>
+                                        <option value='journal'>Journal</option>
+                                        <option value='year'>Year</option>
+                                        <option value='wb_id'>WBPaperID</option>
+                                        <option value='pmid'>PMID</option>
+                                    </FormControl>
+                                </FormGroup>
                             </Col>
                         </Row>
                         <Row>
