d1a3425d174b2114db5e346b052289415485b275
Valerio Arnaboldi <valearna@caltech.edu>
2020-07-13 09:58:39 -0700

    fix(references): link to wbpaper ids

--- frontend/src/pages/ReferenceElement.js
+++ frontend/src/pages/ReferenceElement.js
@@ -12,7 +12,7 @@ class ReferenceElement extends React.Component {
                     <Col sm={4}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + this.props.element.wb_id}>{this.props.element.title}</a></Col>
                     <Col sm={2}>{this.props.element.journal}</Col>
                     <Col sm={1}>{this.props.element.year}</Col>
-                    <Col sm={2}>{this.props.element.wb_id}</Col>
+                    <Col sm={2}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + this.props.element.wb_id}>{this.props.element.wb_id}</a></Col>
                     <Col sm={2}><a target="_blank" rel="noopener noreferrer" href={"https://pubmed.ncbi.nlm.nih.gov/" + this.props.element.pmid}>PMID:{this.props.element.pmid}</a></Col>
                 </Row>
             </Container>
