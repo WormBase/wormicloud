afd4b2ef673800b86bdcd7138929b9d269ec43cb
Valerio Arnaboldi <valearna@caltech.edu>
2021-09-02 16:06:03 -0700

    fix(numCuratedObjects): added link to WB referenced widget
    - direct link to referenced widget on WB paper pages
    - changed all other links to papers (from title and WBPaperID) to open overview widget
    
    https://github.com/WormBase/wormicloud/issues/54

--- frontend/src/components/ReferenceElement.js
+++ frontend/src/components/ReferenceElement.js
@@ -10,15 +10,15 @@ const ReferenceElement = (props) => {
         <Container fluid>
             <Row>
                 <Col sm={1}>{props.element.index}</Col>
-                <Col sm={props.showNumCuratedObjects ? 3 : 4}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + props.element.wb_id}><p dangerouslySetInnerHTML={{__html: props.element.title}}/></a></Col>
+                <Col sm={props.showNumCuratedObjects ? 3 : 4}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + props.element.wb_id + "#0--10"}><p dangerouslySetInnerHTML={{__html: props.element.title}}/></a></Col>
                 <Col sm={1}>{props.element.journal}</Col>
                 <Col sm={1}>{props.element.year}</Col>
                 <Col sm={1}>{props.element.paper_type}</Col>
-                <Col sm={2}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + props.element.wb_id}>{props.element.wb_id}</a></Col>
+                <Col sm={2}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + props.element.wb_id + "#0--10"}>{props.element.wb_id}</a></Col>
                 <Col sm={2}><a target="_blank" rel="noopener noreferrer" href={"https://pubmed.ncbi.nlm.nih.gov/" + props.element.pmid}>PMID:{props.element.pmid}</a></Col>
                 {props.showNumCuratedObjects
                     ?
-                    <Col sm={1}>{props.element.num_curated_entities}</Col>
+                    <Col sm={1}><a target="_blank" rel="noopener noreferrer" href={"https://wormbase.org/resources/paper/" + props.element.wb_id + "#3--10"}>{props.element.num_curated_entities}</a></Col>
                     :
                     null
                 }
